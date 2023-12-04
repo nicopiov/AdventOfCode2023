b5a797b578441e29e7ae8e5edc17f1c6e19be485
Nicolò <nicopiovan@gmail.com>
2023-12-04 13:58:14 +0100

    Day 2 - Part 1 Advent of Code

--- Day 2/Part 1/CubeGame.java
+++ Day 2/Part 1/CubeGame.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class CubeGame {
     public static void main(String[] args){
-    
+        try(Scanner s = new Scanner(System.in)){
+            int sumID = 0;
+            while(s.hasNextLine()){
+                int red = 0, green = 0, blue = 0, gameId = 0;
+                String [] line = s.nextLine().split("(:\s|,\s|;\s)");
+                for(String str : line){
+                    String [] values = str.split(" ");
+                    if(values[0].equals("Game")){
+                        gameId = Integer.parseInt(values[1]);
+                    }
+                    if(values[1].equals("red") && Integer.parseInt(values[0]) >= red){
+                        red = Integer.parseInt(values[0]);
+                    }else if(values[1].equals("green") && Integer.parseInt(values[0]) >= green){
+                        green = Integer.parseInt(values[0]);
+                    }else if(values[1].equals("blue") && Integer.parseInt(values[0]) >= blue){
+                        blue = Integer.parseInt(values[0]);
+
+                    }
+                }
+                if(red <= 12 && green <= 13 && blue <= 14){
+                    sumID += gameId;
+                }
+            }
+            System.out.println(sumID);
+        }
     }
 }
