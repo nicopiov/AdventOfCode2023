016cc48e682048c1b20ab23c942d332e58b9f347
Nicolò <nicopiovan@gmail.com>
2023-12-04 14:03:13 +0100

    Day 2 - Part 2 Advent of Code, and updated Readme

--- Day 2/Part 2/CubePower.java
+++ Day 2/Part 2/CubePower.java
@@ -1,17 +1,14 @@
 import java.util.Scanner;
 
-public class CubeGame {
+public class CubePower {
     public static void main(String[] args){
         try(Scanner s = new Scanner(System.in)){
-            int sumID = 0;
+            int power = 0;
             while(s.hasNextLine()){
-                int red = 0, green = 0, blue = 0, gameId = 0;
+                int red = 0, green = 0, blue = 0;
                 String [] line = s.nextLine().split("(:\s|,\s|;\s)");
                 for(String str : line){
                     String [] values = str.split(" ");
-                    if(values[0].equals("Game")){
-                        gameId = Integer.parseInt(values[1]);
-                    }
                     if(values[1].equals("red") && Integer.parseInt(values[0]) >= red){
                         red = Integer.parseInt(values[0]);
                     }else if(values[1].equals("green") && Integer.parseInt(values[0]) >= green){
@@ -20,11 +17,9 @@ public class CubeGame {
                         blue = Integer.parseInt(values[0]);
                     }
                 }
-                if(red <= 12 && green <= 13 && blue <= 14){
-                    sumID += gameId;
-                }
+                power += (red * green * blue);
             }
-            System.out.println(sumID);
+            System.out.println(power);
         }
     }
 }
